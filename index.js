8f64a35e56dff4a9015002ccd5a355d0d6b35a47
devbaggett <devabaggett@gmail.com>
2018-06-20 17:21:00 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// *** import express library (commonJS module syntax for node) ***
+const express = require("express");
+
+// *** use express library to create express application (single app) ***
+// used to set up configuration that will listen for incoming requests
+// that are being routed to the Express side of the app from the Node side
+// then route those requests on to different route handlers
+const app = express();
+
+// *** create route handler and associate it with given route ***
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+
+// *** wait for Heroku to tell us what our app's port will be ***
+// if there is a Heroku port available, set it for production
+// otherwise, handle case where we are using dev environment
+const PORT = process.env.PORT || 5000;
+
+// *** listen on PORT const ***
+app.listen(PORT);
