545d694f2d932f86c8fc61dada40a7e185bc42b0
devbaggett <devabaggett@gmail.com>
2018-06-21 21:45:49 -0700

    added authentication

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// use schema object to create schema for new collection
+const userSchema = new Schema({
+	// every user will have googleID
+	googleID: String
+});
+
+// CREATE MODEL CLASS
+// makes mongoose aware new collection needs to be created if doesn't exist
+// 1st arg: create new collection 'users'
+// 2nd arg: 'userSchema' we just created
+mongoose.model("users", userSchema);
