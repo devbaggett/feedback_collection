1b3d05f6364ece87255351718d81ec9ab17c00ca
devbaggett <devabaggett@gmail.com>
2018-06-26 19:40:12 -0700

    added dashboard and landing route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,8 @@ const App = () => {
 		<div>
 			<BrowserRouter>
 				<div>
-					<Route path="/" component={Landing} />
+					<Route exact path="/" component={Landing} />
+					<Route path="/surveys" component={Dashboard} />
 				</div>
 			</BrowserRouter>
 		</div>
