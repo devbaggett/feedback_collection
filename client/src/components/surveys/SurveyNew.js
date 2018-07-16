eb3720e36a162ef53dd1dfd04ee36a731f591bf4
devbaggett <devabaggett@gmail.com>
2018-07-16 16:54:54 -0700

    dumped form values on cancel

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,6 +2,7 @@
 
 // CREATE CLASS-BASED COMPONENT
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
@@ -32,4 +33,6 @@ class SurveyNew extends Component {
 	}
 }
 
-export default SurveyNew;
+export default reduxForm({
+	form: "surveyForm"
+})(SurveyNew);
