b7e40061943d05eb079ecbbeafd170f0534023dd
devbaggett <devabaggett@gmail.com>
2018-07-17 10:51:00 -0700

    set up webhook to send data through SendGrid API in dev

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,6 +17,11 @@ module.exports = app => {
 		res.send("Thanks for voting!");
 	});
 
+	app.post("/api/surveys/webhooks", (req, res) => {
+		console.log(req.body);
+		res.send({});
+	});
+
 	// any # of functions can be passed in and they will be executed inline
 	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
